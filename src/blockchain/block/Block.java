00d79cc223e645dcb399221925f25f5102917822
Nikola Gostovikj <nikolagostovic8@gmail.com>
2026-07-25 01:58:34 +0200

    Fixed util flag, made it possible to mine, fixed hashing logic for the structure of the blockchain

--- src/blockchain/block/Block.java
+++ src/blockchain/block/Block.java
@@ -1,4 +1,5 @@
 package blockchain.block;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -7,30 +8,66 @@ import static blockchain.block.Util.calculateHash;
 public class Block {
     private String hash;
     private final String previousHash;
-    private long timeStamp;
+    private final long timeStamp;
     private long nonce;
-    private List<Transactions> transactions;
+    private final List<Transactions> transactions = new ArrayList<>();
+    private String merkleRoot;
 
 
     public Block(String previousHash) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
-        this.hash = calculateHash(previousHash, timeStamp, nonce);
+        this.hash = calculateBlockHash();
+    }
+
+    public boolean addTransaction(Transactions transaction) {
+        if(transaction == null) {
+            return false;
+        }
+        transactions.add(transaction);
+        this.hash = calculateBlockHash();
+        return true;
     }
 
 
-    /*
-    Hash function which uses previous hash, timestamp and nonce
-    Used StringBuffer for concurrency, and SHA-256
-     */
+    private String calculateBlockHash(){
+        return Util.calculateHash(hash + timeStamp + nonce + merkleRoot);
+    }
+
+
 
     public void mineBlock(int difficulty) {
         String target = "0".repeat(difficulty);
+        hash = calculateBlockHash();
         while (!hash.substring(0, difficulty).equals(target)) {
             nonce++;
-            hash = calculateHash(previousHash, timeStamp, nonce);
+            hash = calculateBlockHash();
         }
     }
 
+
+    private String getMerkleRoot() {
+        return merkleRoot;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public long getNonce() {
+        return nonce;
+    }
+
+    public List<Transactions> getTransactions() {
+        return transactions;
+    }
 
 }
--- src/blockchain/block/Util.java
+++ src/blockchain/block/Util.java
@@ -1,20 +1,32 @@
 package blockchain.block;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
-
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 public class Util {
+     /*
+    Hash function which uses previous hash, timestamp and nonce
+    Used StringBuffer for concurrency, and SHA-256
+     */
 
-    //Put all necessary work in a separate class
-
-    public static String calculateHash(String previousHash, long timeStamp, long nonce){
-        String hash = previousHash + Long.toString(timeStamp) + Long.toString(nonce);
+    public static String calculateHash(String input){
         MessageDigest digest = null;
         byte[] hashedBytes = null;
 
         try{
 
             digest = MessageDigest.getInstance("SHA-256");
-            hashedBytes = digest.digest(hash.getBytes());
+            hashedBytes = digest.digest(input.getBytes());
 
         }catch(Exception e){
 
@@ -31,4 +43,54 @@ public class Util {
         return buffer.toString();
     }
 
+    public static KeyPair generateKeyPair(){
+
+        try{
+
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+            keyPairGenerator.initialize(256);
+            return keyPairGenerator.generateKeyPair();
+
+        }catch (Exception e){
+
+            Logger.ERROR.log(e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+
+        }
+    }
+
+    //Sign using private key
+    public static byte[] applyECDSASignature(PrivateKey privateKey, String input){
+        try{
+
+            Signature sig = Signature.getInstance("ECDSA");
+            sig.initSign(privateKey);
+            sig.update(input.getBytes(StandardCharsets.UTF_8));
+            return sig.sign();
+
+        }catch(Exception e){
+
+            Logger.ERROR.log(e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+
+        }
+    }
+    //Verify using public key
+    public static boolean verifyECDSASignature(PublicKey publicKey, String input, byte[] signature){
+        try{
+
+            Signature verifier = Signature.getInstance("ECDSA");
+            verifier.initVerify(publicKey);
+            verifier.update(input.getBytes(StandardCharsets.UTF_8));
+            return verifier.verify(signature);
+
+        }catch(Exception e){
+            Logger.ERROR.log(e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 }
